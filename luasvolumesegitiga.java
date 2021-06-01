906f82fb917e6b2449a5e4554a3737dcffc09267
richardtanoto <74494368+richardtanoto@users.noreply.github.com>
2021-06-01 19:18:29 +0700

    Update luasvolumesegitiga.java

--- luasvolumesegitiga.java
+++ luasvolumesegitiga.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class luasvolumesegitiga  {
     public static void main(String args[]){
-        int tinggi, alas, prisma, luas, volume;
+        float tinggi, alas, prisma, luas, volume;
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan alas dari segitiga");
-        alas = sc.nextInt();
+        alas = sc.nextFloat();
         System.out.println("Masukkan tinggi dari segitiga");
-        tinggi = sc.nextInt();
+        tinggi = sc.nextFloat();
         System.out.println("Masukkan tinggi prisma");
-        prisma = sc.nextInt();
+        prisma = sc.nextFloat();
         luas = (alas*tinggi)/2;
         volume = ((alas*tinggi)/2)*prisma;
         System.out.println("Luas dari Segitiga = "+luas);
         System.out.println("Volume dari Segitiga = "+volume);
+        sc.close();
     }
 }
